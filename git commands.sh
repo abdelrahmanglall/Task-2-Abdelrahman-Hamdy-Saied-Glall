604cbe91630e1d08140c1c5a40c521cf95d7d405
Abdelrahman Glall <abdelrahmanglall21@gmail.com>
2026-06-03 00:32:45 +0300

    Add files via upload

---
+++ git commands.sh
@@ -0,0 +1,15 @@
+git init
+
+git status
+
+echo "DevOps Project 2" > README.md
+
+git add .
+
+git commit -m "Initial commit"
+
+git branch -M main
+
+git remote add origin https://github.com/USERNAME/Task-1-Abdelrahman-Hamdy-Saied-Glall.git
+
+git push -u origin main
